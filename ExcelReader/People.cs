d9b3271d744935d1659fd2946948c4298a1de100
ionutng <ionutngp@gmail.com>
2024-01-05 11:39:25 +0200

    Add a function to get the information from the excel file

---
+++ ExcelReader/People.cs
@@ -0,0 +1,13 @@
+﻿namespace ExcelReader;
+
+internal class People
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Sex { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string JobTitle { get; set; }
+}
